b04b2971cf7fae56538a81618f467996f651a491
G-Audige <gloriaaudige@gmail.com>
2023-12-05 12:21:07 -0500

    Song titles pulled from api and displayed in list

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,4 +1,7 @@
+import axios from 'axios';
 import React from 'react';
+//Hooks
+import { useEffect, useState } from 'react';
 
 function Books() {
   return <div>Books</div>;
--- src/pages/Music.jsx
+++ src/pages/Music.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 
 function Music() {
-  const [data, setData] = useState(null);
+  const [music, setMusic] = useState('null');
   //   const axios = require('axios');
 
   const options = {
     method: 'GET',
     url: 'https://deezerdevs-deezer.p.rapidapi.com/search',
-    params: { q: 'eminem' },
+    params: { q: 'bruno mars' },
     headers: {
       'X-RapidAPI-Key': 'f40703f20dmsh86ac0d16e9c5d33p17af6bjsn62dcd48ef5a9',
       'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
@@ -19,14 +19,26 @@ function Music() {
 
   const getData = async () => {
     const response = await axios.request(options);
-    console.log(response.data);
-    setData(response.data);
+    setMusic(response.data.data);
+    console.log(music[0].title);
   };
-
+  const loaded = () => {
+    return music.map((song) => {
+      return <p>{song.title}</p>;
+    });
+  };
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
   useEffect(() => {
     getData();
   }, []);
-  return <div>Music</div>;
+  return (
+    <div>
+      <p>Music</p>
+      {music ? loaded() : loading()}
+    </div>
+  );
 }
 
 export default Music;
